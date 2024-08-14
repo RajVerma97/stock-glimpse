12d6b70bdc33c15b2449c2ae95ae37e9bf395e92
Rajneesh Verma <rajneeshverma@Rajneeshs-MacBook-Air.local>
2024-08-15 02:19:49 +0530

    feat(commitzen): add commit zen cz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,6 @@
 import Image from "next/image";
 import { Router } from "next/router";
 
-
 export default function Home() {
-  // throw new Error("error in home");
-  
-  
-    
-  
-  ljslfj
-  ljslfj
-  ljslfj
-  ljslfj
-  ljslfj
-  ljslfj
-  
-  
-  k hksjf sjkf ksjdkls  
-
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-
-
-   
-<h1>  </h1>
-  return <h1>hello</h1>
+  return <h1>hello next js commitzen cz added!</h1>;
 }
