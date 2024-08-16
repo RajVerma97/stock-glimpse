eb14c72f7c082cfb102a64391f09ff85a6c91046
Rajneesh Verma <rajneeshverma@Rajneeshs-MacBook-Air.local>
2024-08-16 23:37:15 +0530

    fix(packagejson): change next-jsrc to stock-glimpse

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default async function SignInPage() {
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
       <div className="flex flex-col items-center mt-10 p-10 shadow-md">
-        <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
+        <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In too</h1>
         <GoogleSignInButton />
         <span className="text-2xl font-semibold text-white text-center mt-8">
           Or
