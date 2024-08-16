dd7e5019328e5e69a70ea5b64b5d219be63d29f2
Parmod <p2armod@gmail.com>
2024-08-16 21:42:20 +0530

    pramod committed to rahul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import { Router } from "next/router";
 
 export default function Home() {
-  return <h1>hello next js commitzen cz added!</h1>;
+  return <h1>hello next js commitzen cz added! by pramods  gjgjjg</h1>;
 }
