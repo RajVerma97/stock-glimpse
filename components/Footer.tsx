33e331a86a5705579c69e6f66979db37606fe06a
RajVerma97 <61057997+RajVerma97@users.noreply.github.com>
2024-11-23 23:08:24 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className=" bg-blue-500 mt-10 h-20 flex justify-center items-center">
-      <div className="text-center">2024 Stock Glimpse</div>
+      <div className="text-center">2024  Stock  Glimpse</div>
     </footer>
   )
 }
